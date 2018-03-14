8547a6547c668d54aca6e1b836708732f2080b22
Miguel Muscat <miguelmuscat93@gmail.com>
2018-03-14 10:44:21 +0100

    Fixed typo in doc and make wording more consistent

--- src/ModuleInterface.php
+++ src/ModuleInterface.php
@@ -19,12 +19,12 @@ interface ModuleInterface extends KeyAwareInterface
      * Performs module-specific setup and optionally provides a container.
      *
      * This method SHOULD be used to allow the module to set up and prepare itself for invocation.
-     * If required, the module MAY provide services in a container However, the usage of this container is dependent on
+     * If required, the module MAY provide services in a container. However, the usage of this container is dependent on
      * the consumer and as such there is no guarantee that the container will actually be utilized.
      *
      * @since [*next-version*]
      *
-     * @return ContainerInterface|null A container instance, if any.
+     * @return ContainerInterface|null A DI container instance, if any.
      */
     public function setup();
 
@@ -34,7 +34,7 @@ interface ModuleInterface extends KeyAwareInterface
      * This method MUST be called when the module has been set up and is ready for invocation.
      * A service container MAY be given to this method, which MAY consume its services. This container is not
      * necessarily the same container returned by the instance's `setup()` method. In fact, it is strongly advised to
-     * assume that it is not.
+     * assume that this is not the case.
      *
      * @since [*next-version*]
      *
