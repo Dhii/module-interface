5da7fccbf9dc285b67d94ec9dc1322045b4c922b
Miguel Muscat <miguelmuscat93@gmail.com>
2019-11-14 18:53:52 +0100

    Replaced exception interfaces with concretes

---
+++ src/Exception/ModuleRunException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Modular\Module\Exception;
+
+/**
+ * An exception thrown when a module fails to run.
+ *
+ * @since [*next-version*]
+ */
+class ModuleRunException extends ModuleException
+{
+}
---
+++ src/Exception/ModuleSetupException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Modular\Module\Exception;
+
+/**
+ * An exception thrown when a module fails to complete its setup.
+ *
+ * @since [*next-version*]
+ */
+class ModuleSetupException extends ModuleException
+{
+}
