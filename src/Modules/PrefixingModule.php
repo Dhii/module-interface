ad4f440bfbcf150569fedab2f9eab206f9d096cc
Miguel Muscat <miguelmuscat93@gmail.com>
2020-02-12 10:46:33 +0100

    Added `PrefixingModule` decorator

---
+++ src/Modules/PrefixingModule.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Dhii\Modular\Module\Modules;
+
+use Dhii\Container\DeprefixingContainer;
+use Dhii\Modular\Module\ModuleInterface;
+use Dhii\Modular\Module\ServiceInterface;
+use Psr\Container\ContainerInterface;
+
+/**
+ * A module decorator that can prefix another module's services.
+ *
+ * When a module is decorated with an instance of this class, the module's factories and extensions will have a
+ * prefix applied to them. Furthermore, each service will be modified such that its dependency keys are also prefixed,
+ * if they refer to one of the module's services. References to services outside the module are unaffected.
+ *
+ * The module the run, the original module is given a decorated container that will automatically attempt to add the
+ * prefix. This lets the original module use its original service keys internally, while externally (at an application
+ * level) its service keys remain prefixed.
+ *
+ * Example usage:
+ *  ```
+ *  new PrefixingModule('log/', new LoggingModule());
+ *
+ *  class LoggingModule implements ModuleInterface {
+ *      public function getFactories() : array {
+ *          return [
+ *              'level' => new Value(LogLevel::ERROR),
+ *
+ *              'logger' => new Factory(['level'], ...),
+ *          ];
+ *      }
+ *
+ *      public function getExtensions() : array {
+ *          return [];
+ *      }
+ *
+ *      public function run(ContainerInterface $c) {
+ *          $c->get('logger'); // works!
+ *
+ *          $logger->log('Logger loaded');
+ *      }
+ *  }
+ *
+ *  // In another module or at an application level:
+ *  $c->get('log/logger'); // returns the logger
+ *  $c->get('logger');     // not found!
+ *  ```
+ *
+ * @since [*next-version*]
+ */
+class PrefixingModule implements ModuleInterface
+{
+    /**
+     * @since [*next-version*]
+     *
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * @since [*next-version*]
+     *
+     * @var ModuleInterface
+     */
+    protected $module;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param string          $prefix The prefix to apply to the module's services.
+     * @param ModuleInterface $module The module instance to prefix.
+     */
+    public function __construct(string $prefix, ModuleInterface $module)
+    {
+        $this->prefix = $prefix;
+        $this->module = $module;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFactories() : array
+    {
+        return $this->prefixServices($this->module->getFactories());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExtensions() : array
+    {
+        return $this->prefixServices($this->module->getExtensions());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(ContainerInterface $c)
+    {
+        $this->module->run(new DeprefixingContainer($c, $this->prefix, false));
+    }
+
+    /**
+     * Prefixes a list of services.
+     *
+     * @since [*next-version*]
+     *
+     * @param ServiceInterface[] $services The services to prefix.
+     *
+     * @return array The list of prefixed services.
+     */
+    protected function prefixServices(array $services)
+    {
+        $results = [];
+
+        foreach ($services as $key => $service) {
+            $newKey = $this->prefix . $key;
+
+            $dependencies = $service->getDependencies();
+            $dependencies = array_map(function ($dep) use ($services) {
+                // Only prefix dependencies that exist as services inside the module
+                return array_key_exists($dep, $services)
+                    ? $this->prefix . $dep
+                    : $dep;
+            }, $dependencies);
+
+            $results[$newKey] = $service->withDependencies($dependencies);
+        }
+
+        return $results;
+    }
+}
---
+++ test/unit/Module/PrefixingModuleTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Dhii\Modular\Module\UnitTest\Module;
+
+use Dhii\Modular\Module\ExtensionInterface;
+use Dhii\Modular\Module\FactoryInterface;
+use Dhii\Modular\Module\ModuleInterface;
+use Dhii\Modular\Module\Modules\PrefixingModule as TestSubject;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use ReflectionException;
+
+class PrefixingModuleTest extends TestCase
+{
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testConstruct()
+    {
+        /** @var ModuleInterface|MockObject $module */
+        $module = $this->getMockForAbstractClass(ModuleInterface::class);
+        $subject = new TestSubject("", $module);
+
+        static::assertInstanceOf(ModuleInterface::class, $subject);
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testGetFactories()
+    {
+        $prefix = uniqid('prefix');
+
+        {
+            $key1 = uniqid('key1');
+            $key2 = uniqid('key2');
+
+            /** @var FactoryInterface|MockObject $fac1 */
+            /** @var FactoryInterface|MockObject $fac2 */
+            $fac1 = $this->getMockForAbstractClass(FactoryInterface::class);
+            $fac2 = $this->getMockForAbstractClass(FactoryInterface::class);
+
+            // Factories list
+            $factories = [
+                $key1 => $fac1,
+                $key2 => $fac2,
+            ];
+        }
+        {
+            // Factory dependencies
+            $deps1 = [$key2, 'external1'];
+            $deps2 = ['external2'];
+
+            $fac1->expects(static::once())->method('getDependencies')->willReturn($deps1);
+            $fac2->expects(static::once())->method('getDependencies')->willReturn($deps2);
+        }
+        {
+            // Prefixed dependencies
+            $pDeps1 = [$prefix . $key2, 'external1'];
+            $pDeps2 = ['external2'];
+
+            // New factory mock instances
+            /** @var FactoryInterface|MockObject $facVal1 */
+            /** @var FactoryInterface|MockObject $facVal2 */
+            $newFac1 = $this->getMockForAbstractClass(FactoryInterface::class);
+            $newFac2 = $this->getMockForAbstractClass(FactoryInterface::class);
+
+            // Old factories create new ones
+            $fac1->expects(static::once())->method('withDependencies')->with($pDeps1)->willReturn($newFac1);
+            $fac2->expects(static::once())->method('withDependencies')->with($pDeps2)->willReturn($newFac2);
+        }
+        {
+            /** @var ModuleInterface|MockObject $module */
+            $module = $this->getMockForAbstractClass(ModuleInterface::class);
+
+            $module->expects(static::once())->method('getFactories')->willReturn($factories);
+        }
+
+        $subject = new TestSubject($prefix, $module);
+        $expected = [
+            $prefix . $key1 => $newFac1,
+            $prefix . $key2 => $newFac2,
+        ];
+
+        static::assertSame($expected, $subject->getFactories());
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testGetExtensions()
+    {
+        $prefix = uniqid('prefix');
+
+        {
+            $key1 = uniqid('key1');
+            $key2 = uniqid('key2');
+
+            /** @var ExtensionInterface|MockObject $ext1 */
+            /** @var ExtensionInterface|MockObject $ext2 */
+            $ext1 = $this->getMockForAbstractClass(ExtensionInterface::class);
+            $ext2 = $this->getMockForAbstractClass(ExtensionInterface::class);
+
+            // Factories list
+            $factories = [
+                $key1 => $ext1,
+                $key2 => $ext2,
+            ];
+        }
+        {
+            // Extension dependencies
+            $deps1 = [$key2, 'external1'];
+            $deps2 = ['external2'];
+
+            $ext1->expects(static::once())->method('getDependencies')->willReturn($deps1);
+            $ext2->expects(static::once())->method('getDependencies')->willReturn($deps2);
+        }
+        {
+            // Prefixed dependencies
+            $pDeps1 = [$prefix . $key2, 'external1'];
+            $pDeps2 = ['external2'];
+
+            /** @var ExtensionInterface|MockObject $facVal1 */
+            /** @var ExtensionInterface|MockObject $facVal2 */
+            $newExt1 = $this->getMockForAbstractClass(ExtensionInterface::class);
+            $newExt2 = $this->getMockForAbstractClass(ExtensionInterface::class);
+
+            // Old extensions create new ones
+            $ext1->expects(static::once())->method('withDependencies')->with($pDeps1)->willReturn($newExt1);
+            $ext2->expects(static::once())->method('withDependencies')->with($pDeps2)->willReturn($newExt2);
+        }
+        {
+            /** @var ModuleInterface|MockObject $module */
+            $module = $this->getMockForAbstractClass(ModuleInterface::class);
+
+            $module->expects(static::once())->method('getExtensions')->willReturn($factories);
+        }
+
+        $subject = new TestSubject($prefix, $module);
+        $expected = [
+            $prefix . $key1 => $newExt1,
+            $prefix . $key2 => $newExt2,
+        ];
+
+        static::assertSame($expected, $subject->getExtensions());
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testRun()
+    {
+        $prefix = uniqid('prefix');
+
+        {
+            $key1 = uniqid('key1');
+            $key2 = uniqid('key2');
+
+            $pKey1 = $prefix . $key1;
+            $pKey2 = $prefix . $key2;
+        }
+        {
+            /** @var ModuleInterface|MockObject $module */
+            $module = $this->getMockForAbstractClass(ModuleInterface::class);
+
+            $module->expects(static::once())->method('run')->willReturnCallback(
+                function (ContainerInterface $c) use ($key1, $key2) {
+                    // Assert that the container still has the original keys
+                    static::assertTrue($c->has($key1));
+                    static::assertTrue($c->has($key2));
+                }
+            );
+        }
+        {
+            /** @var ContainerInterface|MockObject $c */
+            $c = $this->getMockForAbstractClass(ContainerInterface::class);
+
+            $c->expects(static::exactly(2))
+              ->method('has')
+              ->withConsecutive([$pKey1], [$pKey2])
+              ->willReturn(true);
+        }
+
+        $subject = new TestSubject($prefix, $module);
+
+        $subject->run($c);
+    }
+}
