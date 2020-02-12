589da5b0bd848acd3c00d764e003f9af6383dea0
Miguel Muscat <miguelmuscat93@gmail.com>
2020-02-12 09:59:06 +0100

    Added `ModularInterface`

---
+++ src/ModularInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dhii\Modular\Module;
+
+/**
+ * Interface for something that is modular and can provide its list of modules.
+ *
+ * @since [*next-version*]
+ */
+interface ModularInterface
+{
+    /**
+     * Retrieves the list of modules.
+     *
+     * @since [*next-version*]
+     *
+     * @return ModuleInterface[]
+     */
+    public function getModules() : array;
+}
